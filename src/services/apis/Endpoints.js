56ffd6148f7605f82c0d4ef7423f84b32f623d28
Navraj Khanal <navrajkhanal61@gmail.com>
2023-02-05 15:53:49 -0600

    Folder Structured

---
+++ src/services/apis/Endpoints.js
@@ -0,0 +1,11 @@
+// For Sample Only
+
+const baseApi = "http://localhost:8080/api/v1/"
+
+const ownerApi = `${baseApi}/owners`
+
+const propertiesApi = `${baseApi}/properties`
+
+export const getOwners = ownerApi
+
+export const getProperties = propertiesApi
