78940c5120d505e6d8cbb44a7a75c859bc114f84
rasikkunwar <rasikkunwar31@gmail.com>
2023-02-06 14:58:12 -0600

    redux implementation

--- src/components/ImageCarousel/ImageCarousel.jsx
+++ src/components/ImageCarousel/ImageCarousel.jsx
@@ -7,55 +7,42 @@ export default function ImageCarousel(){
             <Carousel showArrows={true}>
                 <div>
                     <img src="/banner.jpeg" />
-                    <p className="legend">Legend 1</p>
                 </div>
                 <div>
                     <img src="/banner.jpeg" />
-                    <p className="legend">Legend 2</p>
                 </div>
                 <div>
                     <img src="/banner.jpeg" />
-                    <p className="legend">Legend 3</p>
                 </div>
                 <div>
                     <img src="/banner.jpeg" />
-                    <p className="legend">Legend 4</p>
                 </div>
                 <div>
                     <img src="/banner.jpeg" />
-                    <p className="legend">Legend 5</p>
                 </div>
                 <div>
                     <img src="/banner.jpeg" />
-                    <p className="legend">Legend 6</p>
                 </div>
                 <div>
                     <img src="/banner.jpeg" />
-                    <p className="legend">Legend 6</p>
                 </div>
                 <div>
                     <img src="/banner.jpeg" />
-                    <p className="legend">Legend 6</p>
                 </div>
                 <div>
                     <img src="/banner.jpeg" />
-                    <p className="legend">Legend 6</p>
                 </div>
                 <div>
                     <img src="/banner.jpeg" />
-                    <p className="legend">Legend 6</p>
                 </div>
                 <div>
                     <img src="/banner.jpeg" />
-                    <p className="legend">Legend 6</p>
                 </div>
                 <div>
                     <img src="/banner.jpeg" />
-                    <p className="legend">Legend 6</p>
                 </div>
                 <div>
                     <img src="/banner.jpeg" />
-                    <p className="legend">Legend 6</p>
                 </div>
                 
             </Carousel>
